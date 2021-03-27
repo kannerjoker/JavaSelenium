4119ec9fd7812326a3b2681f6711e04acd0164c9
kannerjoker <2105269619@qq.com>
2021-03-27 19:10:37 +0800

    Selenium_Assert

--- JavaSrc/P7_AssertAttribute/P7_AssertAttribute.java
+++ JavaSrc/P7_AssertAttribute/P7_AssertAttribute.java
@@ -1,5 +1,21 @@
 package P7_AssertAttribute;
 
+import org.testng.Assert;
+
 public class P7_AssertAttribute {
+    public static void main(String[] args) {
+        assertAttribute(Integer.valueOf(1),Integer.valueOf(2));
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {1, 2, 4};
+        assertAttribute(arr1,arr2);
+    }
+
+    public static void assertAttribute(Object n,Object m){
+        try{
+            Assert.assertEquals(n,m,"Exceptions");
+        }catch(Error e){
+            e.printStackTrace();
+        }
+    }
 
 }
